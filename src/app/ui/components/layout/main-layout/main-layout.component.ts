9106a460f3dcf4d4aad8d4eaac01f16c8841cf84
Akash Srivastava <akashom53@gmail.com>
2025-05-24 01:24:04 +0530

    Basic layout acceptable

---
+++ src/app/ui/components/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+  isNavExpanded = false;
+
+  toggleNav() {
+    this.isNavExpanded = !this.isNavExpanded;
+  }
+}
---
+++ src/app/ui/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainLayoutComponent } from "../../components/layout/main-layout/main-layout.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [MainLayoutComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
